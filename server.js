b20c3e0212414dd1f46b8b006aa34738e9838a31
shreeraj <shreerajjadhav19@gmail.com>
2025-02-20 10:49:25 +0530

    ASAP3_endpoint done

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/ping', (req, res) => {
+    res.json({
+        message: "Pong",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`)
+})
